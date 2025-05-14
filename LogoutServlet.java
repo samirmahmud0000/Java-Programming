72dd16a8caa133ad1c54a572222fee3be279b362
Samir Mahmud <94298749+samirmahmud0000@users.noreply.github.com>
2025-05-14 20:54:59 +0600

    Create LogoutServlet.java

---
+++ LogoutServlet.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null) session.invalidate();
+        response.sendRedirect("login.html");
+    }
+}
